3ee62b60f3fc48ca3b1189ff2e2607a630d20b41
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-04-14 22:02:56 +0100

    Template Inheritance and Macros

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']   
+todos = ['☕', '🍌', '🐟']   
 
 @app.route('/')
 def index():
