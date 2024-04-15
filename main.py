5a11ab5e79c29c047c297c5561a45cfef698424c
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-04-15 01:34:19 +0100

    Implementation of Flask-Bootstrap and Flask-WTF

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 Bootstrap = Bootstrap(app)
@@ -7,6 +10,12 @@ Bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['☕', '🍌', '🐟']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,11 +34,12 @@ def index():
 @app.route('/hello')
 def hello_world():
     user_ip = session.get('user_ip')
+    loginForm = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': loginForm
     }
-
     return render_template('hello.html', **context)
 
 if __name__ == '__main__':
